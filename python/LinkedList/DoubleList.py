c270d3f25805dd8af7f31c18cb1cec511f6a604c
ihuangch <1321735387@qq.com>
2020-05-27 23:33:57 +0800

    Add DoubleList

--- python/LinkedList/DoubleList.py
+++ python/LinkedList/DoubleList.py
@@ -45,7 +45,7 @@ class DoubleList():
             self.__head = node
         else:
             current = self.__head
-            while current:
+            while current.next:
                 current = current.next
             current.next = node
             node.prev = current
@@ -54,17 +54,20 @@ class DoubleList():
     def insert(self, index, value):
         if index < 0 or index > self.get_len():
             return False
+        elif index == 0:
+            self.insert_first(value)
         else:
             node = ListNode(value)
             count = 1
             current = self.__head
             while count < index:
                 current = current.next
-
+                count += 1
             node.next = current.next
             current.next.prev = node
             node.prev = current
             current.next = node
+            return True
 
     def search_by_value(self, value):
         if self.is_empty():
@@ -83,7 +86,7 @@ class DoubleList():
         if index < 0 or index > self.get_len():
             return False
         elif index == 0:
-            return self.__head
+            return self.__head.data
         else:
             count = 1
             current = self.__head
@@ -93,12 +96,61 @@ class DoubleList():
             return current.next.data
 
     def delete_by_index(self, index):
-        pass
+        if self.is_empty():
+            return False
+        if index < 0 or index > self.get_len():
+            return False
+        elif index == 0:
+            current = self.__head
+            current.next.prev = None
+            self.__head = current.next
+        else:
+            count = 1
+            current = self.__head
+            while count < index:
+                current = current.next
+                count += 1
+            current.next = current.next.next
+            current.next.prev = current
+            return True 
 
     def delete_by_value(self, value):
-        pass
+        if self.is_empty():
+            return False
+        current = self.__head
+        if current.data == value:
+            current.next.prev = None
+            self.__head = current.next
+        while current.next:
+            if current.next.data == value:
+                if current.next.next:
+                    current.next = current.next.next
+                    current.next.prev = current
+                else:
+                    current.next = None
+            else:
+                current = current.next
+        return True
 
-
+if __name__ == '__main__':
+    doublelist = DoubleList()
+    print(doublelist.is_empty())
+    print(doublelist.get_len())
+    print(doublelist.get_items())
+    print(doublelist.insert_first(1))
+    print(doublelist.get_items())
+    print(doublelist.insert_last(2))
+    print(doublelist.get_items())
+    print(doublelist.insert(1,2))
+    print(doublelist.get_items())
+    print(doublelist.search_by_index(0))
+    print(doublelist.search_by_value(2))
+    print(doublelist.insert(2, 5))
+    print(doublelist.get_items())
+    print(doublelist.delete_by_index(1))
+    print(doublelist.get_items())
+    print(doublelist.delete_by_value(5))
+    print(doublelist.get_items())
             
 
 
